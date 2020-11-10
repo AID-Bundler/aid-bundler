ace17329b4bd018dd0c75519a012b8cc1b0d42c9
TheDudeFromCI <thedudefromci@gmail.com>
2020-11-10 01:17:41 -0700

    Added Standard linting

--- example/subfolder/utils.js
+++ example/subfolder/utils.js
@@ -1,7 +1,7 @@
 // You can import projects using standard require and other libraries as well!
 
-const name = "Steve"
-const color = "Red"
+const name = 'Steve'
+const color = 'Red'
 const num = 27
 
 module.exports = {
